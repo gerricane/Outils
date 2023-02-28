eff3e91c54fb7c482dde93e450f692f3dc6f67d8
gerricane <sebuis16@gmail.com>
2023-02-28 17:21:15 +0100

    Update AnyPagePDF.py

--- AnyPagePDF.py
+++ AnyPagePDF.py
@@ -4,6 +4,7 @@ import os
 
 liste = os.listdir("c:/TMP/test/PDF")
 path = ("c:/TMP/test/PDF/")
+path_output = input("Donnez un nom de fichier de sortie, sans l'extension .pdf: ")
 
 for i in range(len(liste)):
     print(f"{liste[i]}")
@@ -12,10 +13,10 @@ pdfWriter = PdfFileWriter()
 for filename in liste:
     pdfFileObject = open(f"{path}{filename}", "rb")
     pdfReader = PdfFileReader(pdfFileObject)
-    for pageNum in range(pdfReader.numPages):
+    for pageNum in range(0,1):
         pageObj = pdfReader.getPage(pageNum)
         pdfWriter.addPage(pageObj)
 
-pdfOutput =open("c:/TMP/test/PDF/final2.pdf", "wb")
+pdfOutput = open(f"{path}{path_output}.pdf", "wb")
 pdfWriter.write(pdfOutput)
 pdfOutput.close()
