e25808110763e8fdb8358cb244b9b38d7eea6e0c
gerricane <sebuis16@gmail.com>
2023-02-28 17:46:03 +0100

    Update AnyPagePDF.py

--- AnyPagePDF.py
+++ AnyPagePDF.py
@@ -2,15 +2,18 @@ from PyPDF2 import PdfFileReader
 from PyPDF2 import PdfFileWriter
 import os
 
-liste = os.listdir("c:/TMP/test/PDF")
+
 path = ("c:/TMP/test/PDF/")
 page_debut = 0
 page_fin = 1
 path_output = input("Donnez un nom de fichier de sortie, sans l'extension .pdf: ")
 
+'''
 for i in range(len(liste)):
     print(f"{liste[i]}")
+'''
 
+liste = os.listdir("c:/TMP/test/PDF")
 pdfWriter = PdfFileWriter()
 for filename in liste:
     pdfFileObject = open(f"{path}{filename}", "rb")
